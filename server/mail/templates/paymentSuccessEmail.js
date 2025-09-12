cbbfc758c23d46fccc114aa16d26a90298fd213a
dot00Vaibhav <vaibhavtiwari123vt1@gmail.com>
2025-09-12 15:35:07 +0530

    4th semmit

--- server/mail/templates/paymentSuccessEmail.js
+++ server/mail/templates/paymentSuccessEmail.js
@@ -67,7 +67,8 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+            <a href="https://studynotion-sand-sigma.vercel.app"><img class="logo" 
+            src="../../assets/logo-dark.png"
                     alt="StudyNotion Logo"></a>
             <div class="message">Course Payment Confirmation</div>
             <div class="body">
