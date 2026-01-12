26cc72c10968877fa7cb5542a3b0edadc62f0979
dot00Vaibhav <vaibhavtiwari123vt1@gmail.com>
2026-01-12 19:14:33 +0530

    changes in mailSender

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,15 +4,21 @@ const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST || process.env.SMTP_HOST,
+      port: Number(process.env.MAIL_PORT || process.env.SMTP_PORT || 587),
+      secure: Number(process.env.MAIL_PORT || process.env.SMTP_PORT) === 465,
+      
       auth: {
         user: process.env.MAIL_USER || process.env.SMTP_USER || process.env.EMAIL_USER,
         pass: process.env.MAIL_PASS || process.env.SMTP_PASS || process.env.EMAIL_PASS,
       },
-      secure: false,
+
+      tls: {
+        rejectUnauthorized: false,
+      },
     })
 
     let info = await transporter.sendMail({
-      from: `"Studynotion | CodeHelp" <${process.env.MAIL_USER || process.env.SMTP_USER || process.env.EMAIL_USER}>`, // sender address
+      from: `"Studynotion | Vaibhav" <${process.env.MAIL_USER || process.env.SMTP_USER || process.env.EMAIL_USER}>`, // sender address
       to: `${email}`, // list of receivers
       subject: `${title}`, // Subject line
       html: `${body}`, // html body
@@ -20,9 +26,10 @@ const mailSender = async (email, title, body) => {
     // console.log(info.response)
     return info
   } catch (error) {
-    // console.log(error.message)
-    return error.message
+    console.log(error.message)
+    console.error("Error occurred while sending mail ", error)
+    throw new Error("Email could not be sent")
   }
-}
+};
 
 module.exports = mailSender
